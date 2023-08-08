f641f93d4f6382023e1df266c6f51e20743f3dfb
zakir85 <info.zakir85.com>
2023-08-08 19:13:43 +0600

    first commit

---
+++ assignment4.js
@@ -0,0 +1,27 @@
+
+// Problem - 1(basic math)
+
+function cubeNumber(number) {
+}
+
+// Problem - 2
+
+function matchFinder(string1, string2) {
+}
+
+
+// Problem - 3
+
+function sortMaker(arr) {
+}
+
+// Problem - 4
+
+function findAddress(obj) {
+}
+
+
+// Problem - 5
+
+function canPay(changeArray, totalDue) {
+}
